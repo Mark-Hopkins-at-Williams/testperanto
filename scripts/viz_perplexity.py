5acabcddf38fb08cb189d10b453defc8d2185f86
Mark Hopkins <hopkinsm@reed.edu>
2022-05-18 07:06:07 +1200

    Modified viz_perplexity.py.

--- scripts/viz_perplexity.py
+++ scripts/viz_perplexity.py
@@ -16,28 +16,44 @@ def get_perplexity(eval_file):
     return None
 
 def harvest_data(directory):
+    def convert_switch_value(switches):
+        conversions = {'000000': 'jpn', '011101': 'eng'}
+        return conversions[switches]
     subdirs = [f for f in listdir(directory) if isdir(join(directory, f))]
     eval_files = [(subdir, join(join(directory, subdir), 'eval.txt')) for subdir in subdirs
                   if isfile(join(join(directory, subdir), 'eval.txt'))]
     data = []
-    columns = ['switches', 'trial', 'perplexity']
+    columns = ['word order', 'trial', 'perplexity']
     for (key, filename) in eval_files:
         switches, trial = key.split(".")
         perplexity = get_perplexity(filename)
         if perplexity is not None:
-            data.append([switches, int(trial), perplexity])
+            data.append([convert_switch_value(switches), int(trial), perplexity])
     df = pd.DataFrame(data, columns=columns)
     return df
 
+def harvest_nested_data(directory):
+    subdirs = [f for f in listdir(directory) if isdir(join(directory, f))]
+    dfs = [harvest_data(join(directory, subdir)) for subdir in subdirs]
+    for df in dfs:
+        print(df)
+    df = pd.concat(dfs, keys=subdirs)
+    df = df.reset_index(level=0).rename(columns={'level_0': 'technique'})
+    df = df.reset_index(drop=True)
+    print(df)
+    return df
+
 def viz_data(df):
-    sns.set_theme(style="whitegrid")
-    sns.set_palette("Spectral")
-    sns.pointplot(y="perplexity",
-                  x="switches",
-                  data=df, orient="v", join=False,
-                  order=sorted(set(df['switches'])))
+    sns.set_theme(style="darkgrid")
+    sns.set_palette("pastel")
+    sns.catplot(x="perplexity",
+                hue="word order",
+                kind="box",
+                data=df, y="technique")
     plt.xticks(rotation=90)
-    plt.gcf().subplots_adjust(bottom=0.35)
+    plt.gcf().subplots_adjust(left=0.2)
+    plt.gcf().subplots_adjust(bottom=0.2)
+
     plt.show()
 
 def main(directory):
@@ -61,5 +77,6 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dir', required=True,
                         help='experiment directory')
     args = parser.parse_args()
-    dataframe = harvest_data(args.dir)
+    dataframe = harvest_nested_data(args.dir)
+    #dataframe = harvest_data(args.dir)
     viz_data(dataframe)
