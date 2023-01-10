226d86f84af5b27a6b3227d673084ea1621f750f
Mark Hopkins <hopkinsm@reed.edu>
2023-01-10 14:46:00 -0500

    Expanded docstring for amr_str.

--- testperanto/amr.py
+++ testperanto/amr.py
@@ -1,17 +1,27 @@
 def amr_str(tree, indent=""):
-    """This function takes the root node of a tree and prints it out in amr format"""
+    """Takes the root node of a tree and prints it out in amr format.
+    
+    Parameters
+    ----------
+    tree : testperanto.trees.TreeNode
+        the input tree
+    indent : str
+        the base number of spaces to indent each line
+
+    Returns
+    -------
+    str
+        a Penman-style formatting of the tree    
+    """
 
     if len(tree.get_children()) == 0:
-        return indent + tree.get_label()
-    
+        return indent + tree.get_label()    
     child0 = tree.get_child(0)
     assert child0.get_simple_label() == "inst"
     inst0 = child0.get_child(0).get_simple_label()
     res = f"({inst0}"
     for child in tree.get_children()[1:]:
-        #breakpoint()
         res += amr_str(child, indent + " " * 3)
     res += ")"
-
     prefix = f"\n{indent}:{tree.get_simple_label()} " if tree.get_simple_label() != "ROOT" else ""  
     return prefix + res
