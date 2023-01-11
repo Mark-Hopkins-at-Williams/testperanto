732d1e24f5011cd252befa776ee5dde321295b20
Mark Hopkins <hopkinsm@reed.edu>
2023-01-11 14:31:48 -0500

    Revised AMR spec and pretty print.

--- testperanto/amr.py
+++ testperanto/amr.py
@@ -15,13 +15,21 @@ def amr_str(tree, indent=""):
     """
 
     if len(tree.get_children()) == 0:
-        return indent + tree.get_label()    
-    child0 = tree.get_child(0)
-    assert child0.get_simple_label() == "inst"
+        return indent + tree.get_simple_label()
+    child0 = tree.get_child(0)    
+    assert child0.get_simple_label() == "inst", f"tree: {tree}"
     inst0 = child0.get_child(0).get_simple_label()
     res = f"({inst0}"
     for child in tree.get_children()[1:]:
-        res += amr_str(child, indent + " " * 3)
+        if child.get_simple_label() == "mods":
+            if child.get_child(0).get_simple_label() != "-null-":
+                for grandchild in child.get_children():
+                    recursive = amr_str(grandchild.get_child(0), indent + " " * 3)
+                    my_indent = indent + " " * 3
+                    res += f"\n{my_indent}:{grandchild.get_simple_label()} {recursive}"
+        else:
+            recursive = amr_str(child.get_child(0), indent + " " * 3)
+            my_indent = indent + " " * 3
+            res += f"\n{my_indent}:{child.get_simple_label()} {recursive}"
     res += ")"
-    prefix = f"\n{indent}:{tree.get_simple_label()} " if tree.get_simple_label() != "ROOT" else ""  
-    return prefix + res
+    return res
