d55d37e2778a909df1f77aefc114289fbd127614
Nate Lentz <npl1@williams.edu>
2023-01-24 11:46:42 -0500

    Expanded the rulesets for amr.json and english.json and created charts for little prince amr

--- testperanto/amr.py
+++ testperanto/amr.py
@@ -1,5 +1,6 @@
 from testperanto.trees import TreeNode
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 def amr_str(tree, indent=""):
     """Takes the root node of a tree and prints it out in amr format.
@@ -94,15 +95,16 @@ def get_statistics(tree, statistics):
     """A helper method to extract statistics from a treeNode object"""
     for child in tree.get_children():
         if child.get_simple_label() == 'X' and child.get_num_children() > 0:
-            grandchildren = [g.get_simple_label() for g in child.get_children()]
-            statistics[tuple(grandchildren)] += 1
+            grandchildren = [g.get_simple_label() for g in child.get_children() if g.get_simple_label() != 'inst']
+            if grandchildren:
+                statistics[tuple(grandchildren)] += 1
         elif child.get_num_children() > 0:
             get_statistics(child, statistics)
     return statistics
 
 
 def text_stats(path):
-    """Takes an input text file path and returns a list of treeNodeAMRS"""
+    """Takes an input text file path and plots some statistics from the text's AMRS"""
     text_file = open(path, "r")
     data = text_file.read()
     text_file.close()
@@ -114,16 +116,34 @@ def text_stats(path):
         if parse is not None:
             treeNodes.append(parse)
     
-    #for tree in treeNodes[:5]:
-    #    print(tree)
-
     statistics = defaultdict(int)
-    #print(treeNodes)
     for tree in treeNodes:
-        # print(tree)
         get_statistics(tree, statistics)
-    # return treeNodes
-    return statistics
+
+    raw_counts = defaultdict(int)
+    for key, value in statistics.items():
+        for item in key:
+            raw_counts[item] += value
+
+    raw_counts = dict(sorted(raw_counts.items(), key=lambda x:x[1], reverse=True))
+    raw_counts = {str(k):v for (k, v) in raw_counts.items() if v >= 5}
+
+    stats_by_level = dict(sorted(statistics.items(), key=lambda x:x[1], reverse=True))
+    stats_by_level = {str(k):v for (k, v) in stats_by_level.items() if v >= 5}
+
+    fig, (ax1, ax2) = plt.subplots(2)
+    fig.suptitle('Little Prince AMR Breakdown')
+    ax1.bar(stats_by_level.keys(), stats_by_level.values(), width=.9, color='g')
+    ax1.set(xlabel='Sentence Articles', ylabel='count') 
+    ax1.set_xticklabels(stats_by_level.keys(), rotation=45, ha='right', size = "x-small", rotation_mode='anchor')
+
+    ax2.set(xlabel='Articles', ylabel='count')
+    ax2.bar(raw_counts.keys(), raw_counts.values(), width=.9, color='g')
+    ax2.set_xticklabels(raw_counts.keys(), rotation=45, ha='right', size = "x-small", rotation_mode='anchor')
+    fig.subplots_adjust(hspace=0.75)
+
+    plt.show()
+
 
             
 
